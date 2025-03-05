7a78bde30f0d19fef5c9e03e02de0a49c46e93c6
miriam <maviles221@gmail.com>
2025-03-05 11:14:33 -0600

    http response -> about page

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request): 
-    return HttpResponse('<h1>Hello, World!</h1>')
+    return HttpResponse('<h1>Hello, World!</h1>');
+
+def about(request):
+    return HttpResponse('<h1>About Page</h1>');
