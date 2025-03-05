783036c696de290db0dc69f4e1dab7eb2a8e760b
miriam <maviles221@gmail.com>
2025-03-05 12:16:31 -0600

    service page

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+class Service:
+    def __init__(self, name, description, price):
+        self.name = name
+        self.description = description
+        self.price = price
+
+services = [
+    Service('Product 1', 'Description for product 1', 10.00),
+    Service('Product 2', 'Description for product 2', 20.00),
+    Service('Product 3', 'Description for product 3', 30.00),
+]
+
 def home(request): 
-    return render(request, 'home.html');
+    return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html');
+    return render(request, 'about.html')
+
+def service_index(request):
+    return render(request, 'services/index.html', {'services': services})
