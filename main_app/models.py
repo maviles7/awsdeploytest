619c6a6904dac4ddc33e672f96dd89219d6b31ab
miriam <maviles221@gmail.com>
2025-03-05 13:18:34 -0600

    service & contact model - ik they should be seperate commits

--- main_app/models.py
+++ main_app/models.py
@@ -7,3 +7,12 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Contact (models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Service, Contact
 
 def home(request): 
     return render(request, 'home.html')
@@ -8,7 +8,9 @@ def about(request):
     return render(request, 'about.html')
 
 def service_index(request):
+    services = Service.objects.all()
     return render(request, 'services/index.html', {'services': services})
 
 def contact(request):
+    contacts = Contact.objects.all()
     return render(request, 'contact.html', {'contacts': contacts})
