3598571329caf1a210833aeb224e1420b0a2f2a7
miriam <maviles221@gmail.com>
2025-03-06 17:58:46 -0600

    databaser password fix

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'maviles',  # Update this with the correct username
+        'PASSWORD': 'your_correct_password',  # Update this with the correct password
+        'HOST': 'ep-rapid-sky-a5t5h7xd-pooler.us-east-2.aws.neon.tech',
+        'PORT': '5432',
+    }
+}
